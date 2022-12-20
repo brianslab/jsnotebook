42c66e5bbd2719e18755a2565c02b685ab18fbc0
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-19 22:16:18 -0800

    get filepath of notebook file

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const serve = (port, filename, dir) => {
     console.log('Serving traffic on port', port);
-    console.log('Saving/fetching cells from', filename, 'in', dir);
+    console.log('Saving/fetching cells from', filename, 'from', dir);
 };
 exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,4 +1,4 @@
 export const serve = (port: number, filename: string, dir: string) => {
   console.log('Serving traffic on port', port);
-  console.log('Saving/fetching cells from', filename, 'in', dir);
+  console.log('Saving/fetching cells from', filename, 'from', dir);
 };
