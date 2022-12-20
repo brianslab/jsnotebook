dc159d337f100788a75c78985f3915efeb446eb4
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-20 02:47:44 -0800

    refactor package names

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -15,7 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 const path_1 = __importDefault(require("path"));
 const commander_1 = require("commander");
-const local_api_1 = require("local-api");
+const jsnotebook_local_api_1 = require("@brianslab/jsnotebook-local-api");
 const isProduction = process.env.NODE_ENV === 'production';
 exports.serveCommand = new commander_1.Command()
     .command('serve [filename]')
@@ -27,7 +27,7 @@ exports.serveCommand = new commander_1.Command()
     };
     try {
         const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
-        yield (0, local_api_1.serve)(parseInt(options.port), path_1.default.basename(filename), dir, !isProduction);
+        yield (0, jsnotebook_local_api_1.serve)(parseInt(options.port), path_1.default.basename(filename), dir, !isProduction);
         console.log(`Opened ${filename}. Navigate to http://localhost:${options.port} to view and edit this file.`);
     }
     catch (err) {
