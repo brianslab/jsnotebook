7564dbdc13b895c40809a6d71318bd0fdeed6496
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-19 18:14:41 -0800

    specify command line options

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,9 @@
 import { Command } from 'commander';
 
 export const serveCommand = new Command()
-  .command('serve')
+  .command('serve [filename]')
   .description('Open a file for editing')
-  .action(() => {
-    console.log('Preparing to serve a file...');
+  .option('-p, --port <number>', 'port to run server on', '4050')
+  .action((filename = 'notebook.js', options) => {
+    console.log(filename, options);
   });
